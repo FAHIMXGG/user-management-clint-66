0a4dee5a785b21ae5e5d653587aa11231ea403bd
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-06 12:52:43 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,9 @@ function App() {
     .then(res => res.json())
     .then(data => {
       console.log('inside post res', data)
+      const newUsers = [...users, data]
+      setUsers(newUsers);
+      form.reset();
     })
   }
 
